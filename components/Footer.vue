1d805c3aa52b8b15bac1715a0e94d95777bd2568
Tochukwu Franklin Ene <teec3000@gmail.com>
2024-04-01 22:41:18 +0100

    Social Media links added

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,15 @@
             <div class="items-center justify-items-center">
                 <img src="public/assets/images/Logo1.png" class="h-[50px] m-auto" alt="logo" />
                 <div class="flex flex-row gap-[25px] justify-center my-6">
-                    <img src="public/assets/images/insta-icon.svg" class="h-[25px]" />
-                    <img src="public/assets/images/twitter-icon.svg" class="h-[25px]" />
-                    <img src="public/assets/images/facebook.svg" class="h-[25px]" />
+                    <a href="https://www.instagram.com/sunset_realtors">
+                        <img src="public/assets/images/insta-icon.svg" class="h-[25px]" />
+                    </a>
+                    <a class="https://twitter.com/broker_sunset?t=07Nj3WYKpGInESMut5Einw&s=09s">
+                        <img src="public/assets/images/twitter-icon.svg" class="h-[25px]" />
+                    </a>
+                    <a href="https://www.facebook.com/Sunset-Brokers-103757771898576">
+                     <img src="public/assets/images/facebook.svg" class="h-[25px]" />
+                    </a>
                 </div>
                 <p class="font-inter text-[12px] font-[300] text-gray-theme2 sm:text-center">
                     All rights reserved @  sunsetbrokers {{ currentYear }}
