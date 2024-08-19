9c9c04b7d8abed22e43a8487f453ff06e147170e
Tochukwu Franklin Ene <teec3000@gmail.com>
2024-08-19 21:43:04 +0100

    Hot fixes

--- components/WhatWeDo.vue
+++ components/WhatWeDo.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="grid md:grid-cols-2 justify-items-center items-center py-8 px-6 md:px-[57px] md:pb-[80px]" id="services">
+  <section class="grid md:grid-cols-2 md:justify-items-center items-center py-8 px-6 md:px-[57px] md:pb-[80px]" id="service">
     <div class="w-[65%] hidden md:block">
         <img src="public/assets/images/wwd.svg" class="w-screen h-[100%]"/>
     </div>
     <div class="space-y-2 w-100">
-        <h3 class="segoe text-text-theme text-[24px] md:text-[32px] font-semibold">Service (What We Do)</h3>
+        <h3 class="segoe text-text-theme text-[24px] md:text-[32px] font-semibold">Services (What We Do)</h3>
         <ul class="text-black font-normal text-[12px] md:text-[16px] text-justify my-4 font-poppins list-decimal list-outside">
           <li>Real Estate Investment Vehicles</li>
           <li>Easy Housing Scheme</li>
